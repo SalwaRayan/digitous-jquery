c0fdc901ad812c4e631fbf42370fd75d6598bd4b
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-18 17:16:41 +0200

    finished

---
+++ exercice30/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  $("input").keyup(function(){
+    var inputVal = $("input").val()
+    if (inputVal.length > 5) {
+      $("input").addClass("is-valid")
+      $("input").removeClass("is-invalid")
+    } else {
+      $("input").addClass("is-invalid")
+      $("input").removeClass("is-valid")
+    }
+  })
+})
